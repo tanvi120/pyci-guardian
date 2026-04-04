cc2b2ef04dce577d06719a9ad9ef68b342c5d7cd
tanvi120 <tanvi.nevagi@netcorecloud.com>
2026-04-04 16:45:38 +0530

    bug added

--- core/git_utils.py
+++ core/git_utils.py
@@ -3,16 +3,31 @@ import subprocess
 
 def get_git_diff(base_branch="main"):
     try:
-        result = subprocess.run(
+        # 1. committed diff
+        committed = subprocess.run(
             ["git", "diff", f"{base_branch}...HEAD"],
             capture_output=True,
             text=True
-        )
-        return result.stdout
+        ).stdout
+
+        # 2. staged diff
+        staged = subprocess.run(
+            ["git", "diff", "--cached"],
+            capture_output=True,
+            text=True
+        ).stdout
+
+        # 3. unstaged diff (WORKING DIR)
+        unstaged = subprocess.run(
+            ["git", "diff"],
+            capture_output=True,
+            text=True
+        ).stdout
+
+        full_diff = committed + staged + unstaged
+
+        return full_diff
+
     except Exception as e:
         print("Git diff error:", e)
         return ""
-
-
-def create_fix_branch(branch_name="fix/auto-fix"):
-    subprocess.run(["git", "checkout", "-b", branch_name])
